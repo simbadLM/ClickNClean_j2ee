0660d3b9a0574acd594731a36630f6a6cc29ab88
simbadLM <quentin.balloy.pro@gmail.com>
2024-04-21 11:11:26 +0200

    Refactored components to be String instead of PrintWriter + all comps are in components/Page.java

--- src/main/java/components/Page.java
+++ src/main/java/components/Page.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.UserStatus;
 
@@ -12,14 +13,13 @@ public class Page {
     public static String url;
     public static UserStatus sessionHost;
 
-    public static void TopPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public static String TopPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String title = "";
         response.setContentType("text/html;charset=UTF-8");
         if (request.getPathInfo() != null) {
             title = request.getPathInfo().replace("/", "");
         }
-        PrintWriter out = response.getWriter();
-        out.println(
+        return(
             "<!DOCTYPE html>"
             + "<html lang='fr'>"
             + "<head>"
@@ -28,15 +28,72 @@ public class Page {
             +    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
             +        "<title> " + title + "</title>"
             + "</head>"
-            + "<body>" + Header.Render(request, response)
+            + "<body>" + Page.Header(request, response)
         );
     }
 
-    public static void BottomPage(HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();
-        out.println(
+    public static String BottomPage(HttpServletResponse response) throws IOException {
+        return(
             "</body>"
             + "</html>"
         );
     }
+
+    public static String Header(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        UserStatus status = UserStatus.fromString((String)session.getAttribute("status")); // This needs to be casted from Object(session) -> String
+        String headerId = status.toString();
+        boolean connected = false;
+
+        switch (status) {
+            case CLEANER : 
+                    headerId = "cleaner";
+                    connected = true;
+                    break;
+            case OWNER : 
+                    headerId = "owner";
+                    connected = true;
+                    break;
+            case ADMIN: 
+                    headerId = "admin";
+                    connected = true;
+                    break;
+            case UNKNOWN:
+                    headerId = "invite";
+                    break;
+            default:
+                    headerId = "invite";
+        }
+        if (connected) {
+            return(
+                "<header>"
+                + " <div id='" + headerId + "'>"
+                + "     <ul>"
+                + "         <li id = 'logo1'><a href='http://localhost:9090/clickNclean_j2ee/accueil'> <img id='logo' src='./resources/logo_cnc_noBg.png'></a></li>"
+                + "         <li></li>"
+                + "         <li></li>"
+                + "         <li id = 'logo2'><a href='http://localhost:9090/clickNclean_j2ee/accueil'> <img id='logo' src='./resources/logo_cnc_noBg.png'></a></li>"
+                + "     </ul>"
+                + " </div>"
+                + "</header>"
+            );
+        }
+        else {
+            return(
+                "<header>"
+                + " <div id='" + headerId + "'>"
+                + "     <ul>"
+                + "         <li id = 'logo1'><a href='http://localhost:9090/clickNclean_j2ee/accueil'> <img id='logo' src='./resources/logo_cnc_noBg.png'></a></li>"
+                + "         <li></li>"
+                + "         <li></li>"
+                + "         <li id = 'logo2'><a href='http://localhost:9090/clickNclean_j2ee/accueil'> <img id='logo' src='./resources/logo_cnc_noBg.png'></a></li>"
+                + "     </ul>"
+                + " </div>"
+                + "</header>"
+            );
+        }
+    }
+
+
+    
 }
